ec851029a87240bab3df84945034eb9d9e8260d8
cordelliasumantiw <r12627031@ntu.edu.tw>
2025-04-06 18:57:40 +0800

    save

--- Other Expl Var.R
+++ Other Expl Var.R
@@ -1,20 +1,33 @@
 #COMBINE Explanatory Variables Village-level Data FROM 2019-2021#
-folder_services <- "C:/Users/corde/OneDrive/Documents/國立台灣大學 NTU/Thesis/Data/THESIS DATA FIX/Scrape Data"
-file_services <- list.files(path = folder_services, pattern = "*.csv", full.names = TRUE)
+install.packages("tools")
+library(dplyr)
+library(readr)
+library(tools)
+folder_services1 <- "C:/Users/corde/OneDrive/Documents/國立台灣大學 NTU/Thesis/Data/THESIS DATA FIX/Scrape Data/2019"
+file_services1 <- list.files(path = folder_services1, pattern = "*.csv", full.names = TRUE)
+folder_services2 <- "C:/Users/corde/OneDrive/Documents/國立台灣大學 NTU/Thesis/Data/THESIS DATA FIX/Scrape Data/2020"
+file_services2 <- list.files(path = folder_services2, pattern = "*.csv", full.names = TRUE)
+folder_services3 <- "C:/Users/corde/OneDrive/Documents/國立台灣大學 NTU/Thesis/Data/THESIS DATA FIX/Scrape Data/2021"
+file_services3 <- list.files(path = folder_services3, pattern = "*.csv", full.names = TRUE)
 
 #Column Consistency
 read_clean_csv <- function(file) {
   read_csv(file, col_types = cols(.default = "c"))
 }
 
-csv_services <- file_services %>%
+csv_services1 <- file_services1 %>%
   lapply(read_clean_csv) %>%
   bind_rows()
-head(csv_services)
+csv_services2 <- file_services2 %>%
+  lapply(read_clean_csv) %>%
+  bind_rows()
+csv_services3 <- file_services3 %>%
+  lapply(read_clean_csv) %>%
+  bind_rows()
 
 #Add Year, Rename Columns
 library(tools)
-csv_services <- csv_services %>%
+csv_services1 <- csv_services1 %>%
   rename(
     No. = Field1,
     BPS_ID = Phone,
@@ -45,7 +58,103 @@ csv_services <- csv_services %>%
     Village = toTitleCase(Village),
     Year = 2019
     )
-head(csv_services)
+
+csv_services2 <- csv_services2 %>%
+  rename(
+    No. = Field1,
+    BPS_ID = Phone,
+    MoIA_ID = Phone1,
+    Village = Field2,
+    Wom_SupFeed = Field3,
+    Wom_IFA = Field4,
+    Child_SupFeed = Field5,
+    IntSerPost = Field6,
+    Wom_K4 = Field7,
+    VitA = Field8,
+    Compl_Imun = Field9,
+    Zinc = Field10,
+    TG_BBT = Field11,
+    PostNatal_Care = Field12,
+    Nutri_Couns = Field13,
+    BKB = Field14,
+    CleanWater = Field15,
+    Sanitation = Field16,
+    Parenting = Field17,
+    EarlChildEdu = Field18,
+    Health_Insur = Field19,
+    KPM_FDS = Field20,
+    HPK_BPNT = Field21,
+    KRP_Vil = Field22
+  ) %>%
+  mutate(
+    Village = toTitleCase(Village),
+    Year = 2020
+  )
+
+csv_services3 <- csv_services3 %>%
+  rename(
+    No. = Field1,
+    BPS_ID = Phone,
+    MoIA_ID = Phone1,
+    Village = Field2,
+    Wom_SupFeed = Field3,
+    Wom_IFA = Field4,
+    Child_SupFeed = Field5,
+    IntSerPost = Field6,
+    Wom_K4 = Field7,
+    VitA = Field8,
+    Compl_Imun = Field9,
+    Zinc = Field10,
+    TG_BBT = Field11,
+    PostNatal_Care = Field12,
+    Nutri_Couns = Field13,
+    BKB = Field14,
+    CleanWater = Field15,
+    Sanitation = Field16,
+    Parenting = Field17,
+    EarlChildEdu = Field18,
+    Health_Insur = Field19,
+    KPM_FDS = Field20,
+    HPK_BPNT = Field21,
+    KRP_Vil = Field22
+  ) %>%
+  mutate(
+    Village = toTitleCase(Village),
+    Year = 2021
+  )
+
+head(csv_services2)
+
+#Merge All the Services from Year 2019-2021; Change to Numeric; Select Columns
+services_vil <- bind_rows(csv_services1, csv_services2, csv_services3)
+str(services_vil)
+services_vil <- services_vil %>%
+  mutate(
+    Wom_SupFeed = as.numeric(Wom_SupFeed),
+    Wom_IFA = as.numeric(Wom_IFA),
+    Child_SupFeed = as.numeric(Child_SupFeed),
+    IntSerPost = as.numeric(IntSerPost),
+    Wom_K4 = as.numeric(Wom_K4),
+    VitA = as.numeric(VitA),
+    Compl_Imun = as.numeric(Compl_Imun),
+    Zinc = as.numeric(Zinc),
+    TG_BBT = as.numeric(TG_BBT),
+    PostNatal_Care = as.numeric(PostNatal_Care),
+    Nutri_Couns = as.numeric(Nutri_Couns),
+    BKB = as.numeric(BKB),
+    CleanWater = as.numeric(CleanWater),
+    Sanitation = as.numeric(Sanitation),
+    Parenting = as.numeric(Parenting),
+    EarlChildEdu = as.numeric(EarlChildEdu),
+    Health_Insur = as.numeric(Health_Insur),
+    Year = as.integer(Year)
+  )
+head(services_vil)
+services_vil <- services_vil %>%
+  select(Village, Year, Child_SupFeed, VitA, Zinc, Compl_Imun, EarlChildEdu, Wom_SupFeed, Wom_IFA, Wom_K4, CleanWater, Sanitation, IntSerPost, Health_Insur, PostNatal_Care, Nutri_Couns)
+
+#----------------------------------------------------------------
+#Combine Stunt, PO, Expl. Variables (Village-level)
 
 
 #----------------------------------------------------------------
