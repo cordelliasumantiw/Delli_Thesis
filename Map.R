f221e94905c3d9ea29cad097520a8aba9e063cba
cordelliasumantiw <r12627031@ntu.edu.tw>
2025-04-15 22:44:25 +0800

    save

--- Map.R
+++ Map.R
@@ -8,33 +8,36 @@ library(tidyr)
 bound <- st_read("C:/Users/corde/OneDrive/Documents/國立台灣大學 NTU/Thesis/Data/THESIS DATA FIX/R files/Dist Boundaries/idn_admbnda_adm2_bps_20200401.shp")
 bound$ADM2_EN <- toupper(bound$ADM2_EN)
 
-#Calculate the Percentage Change in Years
+str(bound)
 str(po_data)
-#Filter only for 2019, 2020, 2021
-po_filter <- po_data
 
-po_filter <- po_filter %>%
-  pivot_wider(names_from = year, values_from = OP_Area)
+#PO Data 2019 and 2021
+po_data_2019 <- po_data %>% filter(year == 2019)
+po_data_2021 <- po_data %>% filter(year == 2021)
+po_data_1921 <- po_data_2019 %>%
+  select(region, OP_Area) %>%
+  rename(OP_Area_2019 = OP_Area) %>%
+  left_join(po_data_2021 %>% select(region, OP_Area) %>% rename(OP_Area_2021 = OP_Area), by = "region")
+#Percentage Change
+po_data_1921 <- po_data_1921 %>%
+  mutate(Change = ((OP_Area_2021 - OP_Area_2019) / OP_Area_2019) * 100)
 
-#Calculate the Perct. Change
-po_filter <- po_filter %>%
-  mutate(
-    change1920 = ((`2020` - `2019`) / `2019`) * 100,
-    change2021 = ((`2021` - `2020`) / `2020`) * 100,
-    change1921 = ((`2021` - `2019`) / `2019`) * 100,
-  )
-head(po_filter)
+#Change NAs to 0
+po_data_1921 <- po_data_1921 %>%
+  mutate(Change = ifelse(is.na(Change), 0, Change))
 
-#Replace NaNs
-po_filter <- po_filter %>%
-  mutate(
-    change1920 = ifelse(is.nan(change1920), 0, change1920),
-    change2021 = ifelse(is.nan(change2021), 0, change2021),
-    change1921 = ifelse(is.nan(change1921), 0, change1921)
-  )
+na_check_csv_comb <- sapply(po_data_1921, function(x) sum(is.na(x)))
+print(na_check_csv_comb)
+#Merge the Change with Bound
+bound <- bound %>%
+  left_join(po_data_1921 %>% select(region, Change), by = c("ADM2_EN" = "region"))
 
-#Merge back
-po_data_changes <- po_filter %>%
-  left_join(po_filter %>%
-              select(region, change1920, change2021, change1921),
-            by = "region")
+#Data distribution
+summary(bound$Change)
+
+#Plot the Map
+ggplot(data = bound) +
+  geom_sf(aes(fill = Change)) +
+  scale_fill_viridis_c(option = "plasma", name = "% Change") +
+  labs(title = "Percent Change in Palm Oil Expansion: 2019 to 2021") +
+  theme_minimal()
